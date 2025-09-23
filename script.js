94a646dd536b8f2c7a7cbe7246db9ddf963402b0
marcy <marcella.weber@escola.pr.gov.br>
2025-09-23 09:40:17 -0300

    aula 5

--- script.js
+++ script.js
@@ -12,5 +12,16 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+
+let frequencias = {};
+for (let i of palavras) {
+    frequencia[i] =0;
+    for(let j of palavtras) {
+        if(i==j) {
+            frequencias[i]++;
+        }
+    }
+}
     return palavras;
 }
+<!=
