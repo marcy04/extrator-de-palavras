ddfe9e9bd035ed5e403c13c5e679f23bee37e67d
marcy <marcella.weber@escola.pr.gov.br>
2025-09-30 09:38:09 -0300

    .

--- script.js
+++ script.js
@@ -12,15 +12,26 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
-let frequencias = {};
-for (let i of palavras) {
-    frequencia[i] =0;
-    for(let j of palavtras) {
-        if(i==j) {
-            frequencias[i]++;
+    function ordenaPalavra(p1, p2) {
+        return frequencias [p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+    }
+
+    function contaFrequencias(palavras) {
+        let frequencias = {};
+        for (let i of palavras) {
+            frequencias[i] = 0;
+            for (let j of palavras) {
+                if (i == j) {
+                    frequencias[i]++;
+            }
         }
     }
-}
-    return palavras;
-}
+    return frequencias;
+  }
