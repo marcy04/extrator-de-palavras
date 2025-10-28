1970dad2a2d064c5fea893a7e254ea553d1c74af
marcy <marcella.weber@escola.pr.gov.br>
2025-10-28 09:03:10 -0300

    09

--- script.js
+++ script.js
@@ -1,66 +1,55 @@
-const botaoMostraPalavra = document.querySelector ("#botao-palavrachave");
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavra.addEventListener("click", mostraPalavrasChaves);
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    const texto = document.querySelector("#entrada-de-texto").value ;
-    const campoResultado = document.querySelector("#resultado-palavrachave")
-    const palavrasChave = processaTexto (texto);
-    
-    campoResultado.textContent = palavras.join(", ");
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
+
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
-function processaTexto(texto){
+function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
     for (let i in palavras) {
-        palavras[i] = palavras[i].tolowerCase ();
+        palavras[i] = palavras[i].toLowerCase();
     }
 
     palavras = tiraPalavrasRuins(palavras);
 
     const frequencias = contaFrequencias(palavras);
-    let ordenadas = Object.keys (frequencias).sort(ordenaPalavra);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
     function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
     }
 
-   
     console.log(ordenadas);
     return ordenadas.slice(0, 10);
 }
 
-    const frequencias = contaFrequencias(palavras);
-    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
-
-    function ordenaPalavra(p1, p2) {
-        return frequencias [p2] - frequencias[p1];
-    }
-
-    console.log(ordenadas);
-    return ordenadas.slice(0, 10);
-    }
-
-    function contaFrequencias(palavras) {
-        let frequencias = {};
-        for (let i of palavras) {
-            frequencias[i] = 0;
-            for (let j of palavras) {
-                if (i == j) {
-                    frequencias[i]++;
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
             }
         }
     }
     return frequencias;
-  }
+}
 
-  function tiraPalavrasRuins(palavras) {
-    const PALAVRAS_RUINS = new Set({"para", "uma", "nós"});
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
     const palavrasBoas = [];
     for (let palavra of palavras) {
-        if (PALAVRAS_RUINS.has(palavra) && palavra.lenght > 2) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
             palavrasBoas.push(palavra);
         }
     }
     return palavrasBoas;
-  }
+}
